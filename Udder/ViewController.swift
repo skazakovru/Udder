d18f38477503680f899aed520ced87bad70f7ecb
Sergei Kazakov <skazakovru@gmail.com>
2018-10-02 16:59:28 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Sergei Kazakov on 02.10.2018.
+//  Copyright © 2018 Sergei Kazakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
